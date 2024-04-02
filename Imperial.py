fb9ecaaf500e8a9e73013b86e9373c0524c6fd1d
zLEXTRO <164737165+zLEXTRO@users.noreply.github.com>
2024-04-02 14:24:52 +0200

    Add files via upload

--- Imperial.py
+++ Imperial.py
@@ -1,101 +1,143 @@
+import os
+import sys
+import typing 
+from typing import Any
 import discord
 from discord.ext import commands
+from discord import app_commands 
 from discord import Embed
-import typing
+from discord.utils import get
+from discord.ui import Select
+from datetime import datetime
+import asyncio
 
 intents = discord.Intents.all()
-
-bot = commands.Bot(command_prefix= "/", intents=intents)
-
+bot = commands.Bot(command_prefix= "-", intents=intents)
 @bot.event
 async def on_ready():
-    print("Logged in")
+    print(f"logged in as {bot.user.name} ({bot.user.id})")
+        
 
-
+#Unleash the Afterburner
 @bot.command()
 async def rise(ctx):
-    await ctx.send("On your Command")
+    await ctx.send("**On your Command**")
+    await ctx.send('*Latency: {0}*'.format(round(bot.latency, 9)))
 
-#mute command
-@bot.command(name='mute', brief='mute member')
-async def mute(ctx, member: discord.Member):
-     if ctx.message.author.server_permissions.administrator:
-        role = discord.utils.get(member.server.roles, name='Muted')
-        await ctx.add_roles(member, role)
-        embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
-        await ctx.send(embed=embed)
-     else:
-        embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
-        await ctx.send(embed=embed)
+#sync slash commands
+@bot.hybrid_command()
+async def sync(ctx: commands.Context):
+	await ctx.send('Synced')
+	await bot.tree.sync()
+
+#custom TimeConverter
+def TimeConverter(time_str):
+    if time_str[-1] == 's':
+        return int(time_str[:-1])
+    elif time_str[-1] == 'm':
+        return int(time_str[:-1]) * 60
+    elif time_str[-1] == 'h':
+        return int(time_str[:-1]) * 3600
+    elif time_str[-1] == 'd':
+        return int(time_str[:-1]) * 86400
+    else:
+        raise commands.BadArgument("Invalid time format. Use [1d/h/m/s] format.")
+
+#tempmute command
+@bot.hybrid_command(name='mute', description='Mutes the mentioned user for the given amount of time.')
+async def mute(ctx: commands.Context,  member:discord.Member, *, time:TimeConverter = None, reason=None):
+"""Mutes a member for the specified time in [1d/h/m/s] format ex: -mute @Someone 1d"""
+            role = discord.utils.get(ctx.guild.roles, name="Muted")
+            memRole = discord.utils.get(ctx.guild.roles, name='Member')
+            await member.remove_roles(memRole)
+            await member.add_roles(role)
+            await ctx.send(("Muted {} for {}s" if time else "Muted {}").format(member, time))
+            if time:
+                await asyncio.sleep(time)
+                await member.add_roles(memRole)
+                await member.remove_roles(role)
+                
+#unmute command
+@bot.hybrid_command(name='unmute', description='Unmutes the mentioned user.')
+async def unmute(ctx: commands.Context, member:discord.Member, *, reason=None):
+            role = discord.utils.get(ctx.guild.roles, name="Muted")
+            memRole = discord.utils.get(ctx.guild.roles, name='Member')
+            await member.remove_roles(role)
+            await member.add_roles(memRole)
+            await ctx.send(f'Unmuted {member}.')
         
 #kick command
-@bot.command(name='kick', brief='kick member')
+@bot.hybrid_command(name='kick', brief='kick member')
 
-async def kick(ctx, member: discord.Member, *, reason=None):
+async def kick(ctx: commands.Context, member: discord.Member, *, reason=None):
 
     await member.kick(reason=reason)
 
     await ctx.send(f' {member} has been kicked out.')
-    
-    
+        
+#Give User IDs from the Ban List
+@bot.command(name="banlist")
+async def banlist(ctx):
+    guild = ctx.guild
+    if guild:
+        banned_ids = await get_banned_user_ids(guild)
+        if banned_ids:
+            await ctx.send(f"**Banned User IDs:**\n{', '.join(str(uid) for uid in banned_ids)}")
+        else:
+            await ctx.send("This server doesn't have anyone banned (yet)")
+async def get_banned_user_ids(guild):
+    banned_users = await guild.bans()
+    return [user.user.id for user in banned_users]
 
 #Ban Command 
-@commands.has_permissions(ban_members=True)
-@bot.command(name='ban', brief='ban member')
-async def ban(ctx, user: discord.Member, *, reason="No reason provided"):
-        await user.ban(reason=reason)
-        ban = discord.Embed(title=f" {user.name} has been demolished :punch: ", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
-        await ctx.message.delete()
-        await ctx.channel.send(embed=ban)
-        await user.send(embed=ban)
+@bot.hybrid_command(name='ban', brief='ban member')
+async def ban(ctx: commands.Context, user: discord.Member, *, reason="No reason provided"):
+    if isinstance(ctx, discord.ApplicationContext):
+        await ctx.defer()
+    await user.ban(reason=reason)
+    ban = discord.Embed(title=f"{user.name} has been demolished :punch:", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
+    if isinstance(ctx, discord.ApplicationContext):
+        await ctx.respond(embed=ban)
+    else:
+        await ctx.send(embed=ban)
+
     
 #unban command"
-@bot.command(name='unban', brief='unban member')
-async def unban(ctx, *, member):
-    banned_users = await ctx.guild.bans()
-    member_name, member_discriminator = member.split("#")
+@bot.hybrid_command(name='unban', brief='unban member')
+async def unban(ctx: commands.Context, *, user_id: int, reason="No reason provided"):
+    user = await bot.fetch_user(user_id)
+    await ctx.guild.unban(user, reason=reason)
+    unban = discord.Embed(title=f"{user.name} has been given another chance.", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
+    if isinstance(ctx, discord.ApplicationContext):
+        await ctx.respond(embed=unban)
+    else:
+        await ctx.send(embed=unban)
 
-    for ban_entry in banned_users:
-        user = ban_entry.user
 
-        if (user.name, user.discriminator) == (member_name, member_discriminator):
-            await ctx.guild.unban(user)
-            await ctx.send(f'Unbanned {user.mention}, Welcome Back.')
-            return
-    await ctx.send("User not found in the ban list.")
-
-@commands.has_permissions(manage_messages=True)
-@bot.command(name='purge', brief='Deletes a specified number of messages')
-async def purge(ctx, amount: int):
-  
-  # Delete the specified number of messages
+# Delete the specified number of messages
+@bot.hybrid_command(name='purge', brief='Deletes a specified number of messages')
+async def purge(ctx: commands.Context, amount: int):
   deleted = await ctx.channel.purge(limit=amount)
   if len(deleted) == 0:
-    
-    # If no messages were deleted, create an embed message with a custom color and text
     embed = discord.Embed(title='Purge complete', color=0xFFFF00)
     embed.description = 'No messages were deleted'
-    # Set the user's profile picture as the thumbnail of the embed
     embed.set_thumbnail(url=ctx.author.avatar.url)
-    
-    # Send the embed message
     await ctx.send(embed=embed)
   else:
-    
-    # Create an embed message with a custom color and text
     embed = discord.Embed(title='Purge complete', color=0xFFFF00)
     if len(deleted) == 1:
-      
-      # singular & plural purge message(s)
       embed.description = '1 message was deleted'
-    else:
-      
+    else: 
       embed.description = f'{len(deleted)} messages were deleted'
-      
-    # Set the user's profile picture as the thumbnail of the embed
     embed.set_thumbnail(url=ctx.author.avatar.url)
-    
-    # Send the embed message
     await ctx.send(embed=embed)
-             
-bot.run('INSERT BOT TOKEN')	
+
+
+#give role (optional)
+@bot.hybrid_command(pass_context=True)
+async def honor(ctx: commands.Context, user: discord.Member, role: discord.Role):
+    await user.add_roles(role)
+    await ctx.send(f"🎖️** {user.name}**, in recognition of your contributions, you have been bestowed the role of ~ {role.mention}   May this honor reflect the zenith of your dedication and commitment to our community")      
+    
+
+bot.run('BOT_TOKEN')	
--- README.md
+++ README.md
@@ -1,24 +1,33 @@
-# Multi-Purpose-Discord-Bot-Project-Imperial
+# Imperial Discord Bot
 
-Welcome to the Imperial Discord Bot repository! This repository showcases the code of my private Discord bot, Imperial. It's an ambitious project aimed at creating a comprehensive multi-purpose bot designed to enhance the Discord experience. Currently equipped with essential moderation tools, Imperial is on a path of continuous development to integrate a wide array of features.
+Welcome to the Imperial Discord Bot repository! This repository showcases the code of my upcomming Discord bot, Imperial. It's an ambitious project aimed at creating a comprehensive multi-purpose bot designed to enhance the Discord experience. Currently equipped with essential moderation tools, Imperial is on a path of continuous development to integrate a wide array of features.
 
 **Please note**: The code and features in this repository are subject to change as the bot evolves. Additionally, inviting Imperial to your own server is not currently possible until it is properly set up and has a sufficient feature set to ensure a great user experience. The code provided is a preview, demonstrating the implementation of the commands and is not the final version.
 
 
 ## Current Features
+- **Kick Members**: punish bad behavior by throwing members out of the Server.
 - **Ban Members**: Keep your server safe by banning disruptive members.
 - **Unban Members**: Allow members another chance by lifting bans when appropriate.
 - **Mute Members**: Temporarily silence members to maintain order in your channels.
 - **Purge Messages**: Clean up your chat by bulk-deleting messages quickly and efficiently.
 
-## Upcoming Features
-- **Voice Channel (VC) Support**: Manage and interact with voice channels in innovative ways.
-- **AI Support**: Harness the power of artificial intelligence for auto-moderation, user engagement, and more.
+## --NEW-- [UPDATE #1]
+- **Code Improvements**
+- **Added /Slash feature to most of the Commands**
+- **Sync** you can basically sync your newly added commands.
+- **tempmute**: Updated the "mute" command to support TimeConverter.
+- **Banlist**: get user IDs of members in the banlist.
+- **Unmute**: unmute members.
 
+## Upcoming Features Roadmap
+- **Voice Channel (VC) Support**: Manage and interact with voice channels.
+- **AI Support**: auto-moderation, user engagement, and more.(NOTE: might train ai by myself for a better user experience)
 - **MUCH MORE!**: a wide range of Features from moderation, to Entertainment, all the way to Artificial Intelligence.
 
+
 Imperial is more than just a bot.
-It's a community-driven project that grows with your support and feedback. Stay tuned for updates as we expand Imperial's capabilities and redefine what a Discord bot can do!
+It's a community-driven project that grows with your support and feedback. Stay tuned for updates as I expand Imperial's capabilities and redefine what a Discord bot can do.
 
 ## Contributing
-We welcome contributions from the community. Whether it's feature suggestions, bug reports, or code contributions, your input is valuable.
+I welcome contributions from the community. Whether it's feature suggestions, bug reports, or code contributions, your input is valuable.
